dbdf4c4bf55187636af11adc3538bf757922403c
imAnonAmi <mcintireaw@gmail.com>
2020-04-16 17:23:25 -0700

    Burger names not displaying in list. All other functions, plus a delete, are working.

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -17,10 +17,12 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burgers", function(req, res) {
+ console.log(req.body);
   burger.create([
     "burger_name", "devoured"
   ], [
-    req.body.burger_name, req.body.devoured
+    
+    req.body.name, req.body.devoured
   ], function(result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
